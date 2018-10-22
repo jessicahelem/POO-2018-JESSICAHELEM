3071143d68d3a733493317444e5907cdc35aef6d
jessica helem <jessicahelem.s@hotmail.com>
2018-10-22 16:28:06 -0300

    Atividade F

---
+++ src/model/Repositorio.java
@@ -0,0 +1,13 @@
+package model;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Repositorio {
+	
+
+		public String nome;
+		public static ArrayList<Arquivo>trackedFiles;
+		public ArrayList<Arquivo>untracked_files;
+
+}
